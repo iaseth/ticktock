b77fb7387c54d0ee6101f32e0e8047dd87ee1e9c
Ankur Seth <iankurseth@gmail.com>
2022-02-12 18:58:22 +0530

    Displaying current time at center

--- src/TickTock/CenterText.js
+++ src/TickTock/CenterText.js
@@ -1,17 +1,27 @@
 
 
 
+function padZero (x) {
+	return (x < 10) ? "0" + x : x;
+}
+
 export default function CenterText ({
-	//
+	date
 }) {
+
+	let hours = padZero(date.getHours());
+	let minutes = padZero(date.getMinutes());
+	let seconds = padZero(date.getSeconds());
 
 	return (
 		<div className="CenterText absolute flex h-full w-full text-white">
 			<div className="m-auto">
 				<div className="text-4xl">
-					<span>08</span>
+					<span>{hours}</span>
 					<span>:</span>
-					<span>48</span>
+					<span>{minutes}</span>
+					<span>:</span>
+					<span>{seconds}</span>
 				</div>
 			</div>
 		</div>
--- src/TickTock/TickTock.js
+++ src/TickTock/TickTock.js
@@ -1,10 +1,20 @@
-import Markings from './Markings';
-import Dial from './Dial';
+import React from 'react';
+
 import CenterText from './CenterText';
+import Dial from './Dial';
+import Markings from './Markings';
 
 
 
 export default function TickTock () {
+	let [date, setDate] = React.useState(new Date());
+
+	React.useEffect(() => {
+		const id = setInterval(function () {
+			setDate(new Date());
+		}, 1000);
+		return () => clearInterval(id);
+	}, []);
 
 	return (
 		<div className="min-h-screen bg-indigo-500 select-none flex flex-col overflow-hidden">
@@ -17,7 +27,7 @@ export default function TickTock () {
 						<Dial radius={140} bg="bg-indigo-700" />
 						<Markings radius={120} n={12} group_length={3} />
 						<Dial radius={100} bg="bg-indigo-600" />
-						<CenterText />
+						<CenterText date={date} />
 					</div>
 				</div>
 			</div>
