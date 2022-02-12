4be7d14f3144e6fc7422ab45202abfcc0fb82ab7
Ankur Seth <iankurseth@gmail.com>
2022-02-12 19:34:58 +0530

    Moved MarkingDot code into own file

--- src/TickTock/MarkingDot.js
+++ src/TickTock/MarkingDot.js
@@ -1,11 +1,24 @@
 
 
 
-export default function MarkingDot () {
+export default function MarkingDot ({
+	radius, group_length, dotSizePx, step, index
+}) {
+
+	let markClass = "MarkingDot absolute rounded-full";
+	markClass += (index % group_length === 0) ? " bg-indigo-900" : " bg-indigo-300";
+	let degree = index * step;
+	let radian = degree * Math.PI / 180;
+	let left = Math.round(radius * Math.sin(radian));
+	let top = Math.round(-radius * Math.cos(radian));
+	let style = {
+		width: dotSizePx,
+		height: dotSizePx,
+		left: left + "px",
+		top: top + "px"
+	};
 
 	return (
-		<div className="MarkingDot">
-			<h2>MarkingDot</h2>
-		</div>
+		<div className={markClass} style={style}></div>
 	);
 }
--- src/TickTock/MarkingDots.js
+++ src/TickTock/MarkingDots.js
@@ -13,23 +13,11 @@ export default function MarkingDots ({
 	let dotSizePx = dotSize + "px";
 	let step = 360 / n;
 
-	let markingItems = Array(n).fill(0).map((x, i) => {
-		let markClass = "MarkingDot absolute rounded-full";
-		markClass += (i % group_length === 0) ? " bg-indigo-900" : " bg-indigo-300";
-		let degree = i * step;
-		let radian = degree * Math.PI / 180;
-		let left = Math.round(radius * Math.sin(radian));
-		let top = Math.round(-radius * Math.cos(radian));
-		let style = {
-			width: dotSizePx,
-			height: dotSizePx,
-			left: left + "px",
-			top: top + "px"
+	let markingItems = Array(n).fill(0).map((x, index) => {
+		let props = {
+			radius, group_length, dotSizePx, step, index
 		};
-
-		return (
-			<div key={i} className={markClass} style={style}></div>
-		);
+		return <MarkingDot key={index} {...props} />;
 	});
 
 	let dotContainerStyle = {
