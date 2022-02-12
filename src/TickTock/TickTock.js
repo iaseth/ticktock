df13146466b5920414099d3675e2c3363d9e0bf3
Ankur Seth <iankurseth@gmail.com>
2022-02-13 04:27:23 +0530

    Now Shift+H toggles the footer * implemented handleKeydown()

--- src/TickTock/TickTock.js
+++ src/TickTock/TickTock.js
@@ -9,6 +9,7 @@ import SecondsCircle from './SecondsCircle';
 
 export default function TickTock () {
 	let [date, setDate] = React.useState(new Date());
+	let [showFooter, setShowFooter] = React.useState(true);
 
 	React.useEffect(() => {
 		const id = setInterval(function () {
@@ -17,9 +18,30 @@ export default function TickTock () {
 		return () => clearInterval(id);
 	}, []);
 
+	function handleKeydown (event) {
+		if (event.ctrlKey) return;
+		if (event.altKey) return;
+
+		if (event.shiftKey) {
+			let key = event.key.toUpperCase();
+			switch (key) {
+				case "H": setShowFooter(showFooter => !showFooter); break;
+				default: return;
+			}
+		}
+	}
+
+	React.useEffect(() => {
+		document.addEventListener("keydown", handleKeydown, false);
+
+		return function cleanUp () {
+			document.removeEventListener("keydown", handleKeydown, false);
+		}
+	}, []);
+
 	return (
-		<div className="min-h-screen bg-indigo-500 select-none flex flex-col overflow-hidden">
-			<div className="flex grow md:min-h-screen py-12">
+		<div className="bg-indigo-500 select-none flex flex-col overflow-hidden">
+			<div className="flex grow md:min-h-screen py-12 min-h-screen">
 				<div className="m-auto bg-red-300 w-0 h-0 scale-[0.50] md:scale-100">
 					<div className="w-[500px] h-[500px] m-auto bg-indigo-600 rounded-full flex relative -translate-y-1/2 -translate-x-1/2">
 						<MarkingDots radius={270} />
@@ -40,11 +62,11 @@ export default function TickTock () {
 					</div>
 				</div>
 			</div>
-			<footer className="">
+			{showFooter && <footer className="">
 				<div className="text-white text-center mx-auto bg-indigo-600 px-12 py-20">
-					<a href="https://github.com/iaseth/ticktock" target="_blank" rel="noreferrer" className="text-2xl px-4 py-3 text-sm bg-indigo-700 duration-300 hover:bg-indigo-800">iaseth | ticktock</a>
+					<a href="https://github.com/iaseth/ticktock" target="_blank" rel="noreferrer" className="text-base font-bold px-4 py-3 text-sm bg-indigo-700 duration-300 hover:bg-indigo-800">iaseth | ticktock</a>
 				</div>
-			</footer>
+			</footer>}
 		</div>
 	);
 }
